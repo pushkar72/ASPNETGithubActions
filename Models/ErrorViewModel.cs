454159345009fb3e184d231069516a1d53c7ba94
Pushkar Jethwa <pushkarjethwa@outlook.com>
2022-07-17 11:05:52 +0000

    Project Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OHIApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
